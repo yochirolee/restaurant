b05b25d14466be51b3cdd2a9c52b66cc20481a94
yochirolee <yleecruz@gmail.com>
2020-06-10 20:20:59 -0500

    Updates

---
+++ precache-manifest.d294c529d9706462bf8604fb4943ba4e.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c709d9094263c23ffb1a3eb1a0fe15cf",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "1c088492aa748d243a4f",
+    "url": "/restaurant/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "515f4fb9c6668ccf695a",
+    "url": "/restaurant/static/css/main.f4f8b69c.chunk.css"
+  },
+  {
+    "revision": "1c088492aa748d243a4f",
+    "url": "/restaurant/static/js/2.cfada50f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant/static/js/2.cfada50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515f4fb9c6668ccf695a",
+    "url": "/restaurant/static/js/main.dd3c8420.chunk.js"
+  },
+  {
+    "revision": "b3153e1c7af9198e2669",
+    "url": "/restaurant/static/js/runtime-main.d81fd9cc.js"
+  },
+  {
+    "revision": "f59b2a87b6fe2563981b316a949bca2d",
+    "url": "/restaurant/static/media/aji-picante.f59b2a87.svg"
+  },
+  {
+    "revision": "381d6250817ec237ffd5bf3b442c29c7",
+    "url": "/restaurant/static/media/demo1-0901996381-1.381d6250.jpg"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/restaurant/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/restaurant/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/restaurant/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/restaurant/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/restaurant/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/restaurant/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/restaurant/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/restaurant/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/restaurant/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/restaurant/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/restaurant/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/restaurant/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/restaurant/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/restaurant/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/restaurant/static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "e126423d9a256ff6db5156175e8e868a",
+    "url": "/restaurant/static/media/hoja.e126423d.svg"
+  },
+  {
+    "revision": "20c2322dc1ac5e25fc0d628d924b4da4",
+    "url": "/restaurant/static/media/logo.20c2322d.png"
+  },
+  {
+    "revision": "c095e1776b53f9e12870ca7d2b58a297",
+    "url": "/restaurant/static/media/person-pizza.c095e177.jpg"
+  }
+]);
