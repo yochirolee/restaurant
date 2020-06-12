1b1d7473076df0da727e547639a5a13567341578
yochirolee <yleecruz@gmail.com>
2020-06-12 00:53:11 -0500

    facebook login changes

--- src/components/Home/init.jsx
+++ src/components/Home/init.jsx
@@ -14,12 +14,9 @@ export default function Init() {
    //facebookLogin.isLogin
 
   return (facebookLogin.isLogin) ? (
-    <Router>
-      <Switch>
-          <Route path="/" exact component={ProductsPage} /> 
-          <Route path="/payment" exact  component={PaymentPage} /> 
-       </Switch>
-     </Router>
+    
+          <ProductsPage/> 
+    
     
   ) : (
       <HomePage/>
